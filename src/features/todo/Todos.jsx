21dde996330bf489ba69a5ffeeebc3b6d067439f
Shanu30 <ksechenari@gmail.com>
2022-04-29 17:14:19 +0530

    update

--- src/features/todo/Todos.jsx
+++ src/features/todo/Todos.jsx
@@ -10,48 +10,42 @@ import Box from "@mui/material/Box";
 import { Button } from "@mui/material";
 
 const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
-
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Todos = () => {
-    const [open, setOpen] = React.useState(false);
-    // const [pop, setPop] = React.useState();
-    
-    const [postTitle, setPostTitle] = useState("")
-    const [postBody, setPostBody] = useState("")
-    const dispatch = useDispatch();
-    const [title, setTitle] = useState("");
-    const { todos, isLoading, error } = useSelector((state) => state.todos);
+  const [open, setOpen] = React.useState(false);
 
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const { todos, isLoading, error } = useSelector((state) => state.todos);
 
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    const handlecreate = async(postTitle, postBody) => {
-        const payload = {
-            title:postTitle,
-            body:postBody
-        }
-        // await axios.post(`http://localhost:3000/data`,payload)
-        dispatch(addTodo(payload))
-        dispatch(getAllTodos())
-        setOpen(false)
-    }
-
+  const handlecreate = async (postTitle, postBody) => {
+    const payload = {
+      title: postTitle,
+      body: postBody,
+    };
+    dispatch(addTodo(payload));
+    dispatch(getAllTodos());
+    setOpen(false);
+  };
 
   useEffect(() => {
     dispatch(getAllTodos());
   }, [dispatch]);
- 
 
   if (isLoading) {
     return <CircularProgress style={{ margin: "auto", marginTop: "100px" }} />;
@@ -59,14 +53,44 @@ const Todos = () => {
     return <h1>Error in request</h1>;
   } else {
     return (
-        
-
-
       <div>
         <div>
           <Button onClick={handleOpen}>
             <h2>Create New</h2>
           </Button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <TextField
+                id="outlined-basic"
+                placeholder="postTitle"
+                variant="outlined"
+                value={postTitle}
+                onChange={(e) => setPostTitle(e.target.value)}
+              />
+              <TextField
+                id="outlined-basic"
+                placeholder="postBody"
+                variant="outlined"
+                value={postBody}
+                onChange={(e) => setPostBody(e.target.value)}
+              />
+              <br />
+              <br />
+
+              <Button
+                onClick={() => handlecreate(postTitle, postBody)}
+                variant="contained"
+              >
+                Add
+              </Button>
+            </Box>
+          </Modal>
         </div>
         <hr />
         <br />
@@ -76,40 +100,6 @@ const Todos = () => {
           variant="outlined"
           onChange={(e) => setTitle(e.target.value)}
         />
-
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            
-            <TextField
-              id="outlined-basic"
-              placeholder="postTitle"
-              variant="outlined"
-              value={postTitle}
-              onChange={(e) => setPostTitle(e.target.value)}
-            />
-            <TextField
-              id="outlined-basic"
-              placeholder="postBody"
-              variant="outlined"
-              value={postBody}
-              onChange={(e) => setPostBody(e.target.value)}
-            />
-            <br />
-            <br />
-
-            <Button
-              onClick={()=>handlecreate(postTitle, postBody)}
-              variant="contained"
-            >
-              Add
-            </Button>
-          </Box>
-        </Modal>
 
         <br />
         <br />
